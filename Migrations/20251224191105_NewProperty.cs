e6b18af5aaa4e80a519266b402311514fa1880ee
Cheikh Ahmad Mbacke <cheikhahmadmbacke74@email.com>
2025-12-24 23:52:56 +0000

    Migration

---
+++ Migrations/20251224191105_NewProperty.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FusionPayProxy.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerEmail",
+                table: "Transactions",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReturnUrl",
+                table: "Transactions",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomerEmail",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "ReturnUrl",
+                table: "Transactions");
+        }
+    }
+}
